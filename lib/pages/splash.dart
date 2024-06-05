d0bed50c0884a13ecd296448dea4dec1f7f4bb98
Al Azad <alazad214@gmail.com>
2024-06-05 11:57:24 +0600

    splash screen added

---
+++ lib/pages/splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:quizguru/pages/home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.offAll(() => const Home());
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+        body: ListView(
+      children: [
+        Container(
+          height: screenSize.height / 1,
+          alignment: Alignment.center,
+          child: Center(
+              child: Container(
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(shape: BoxShape.circle),
+                  child: Image.asset("assets/images/logo.png", height: 100))),
+        )
+      ],
+    ));
+  }
+}
